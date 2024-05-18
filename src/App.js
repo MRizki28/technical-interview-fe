32053e3aa8aabb3bd159c7664ca9bea8e41fb8ca
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-18 20:17:37 +0800

    finish;

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Select from 'react-select';
+
 
 function App() {
+  const [product, setProduct] = useState([])
+  const [productClick, setProductClick] = useState("")
+  const [productSubmit, setProductSubmit] = useState("");
+
+  const getProduct = async () => {
+    try {
+      const response = await axios.get('https://dummyjson.com/products')
+      const responseData = response.data.products
+      const results = responseData.map(data => ({
+        value: data.title,
+        label: data.title
+      }))
+
+      setProduct(results.sort((a, b) => a.value.localeCompare(b.label)));
+    } catch (error) {
+      console.log(error);
+    };
+  }
+
+  useEffect(() => {
+    getProduct()
+  }, [])
+
+
+  const handleSubmit = () => {
+    setProductSubmit(productClick)
+  }
+
+  const handleChange = (selected) => {
+    setProductClick(selected.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: "30px", textAlign: "center" }}>
+      <Select options={product} onChange={handleChange} />
+      <button type='button' style={{ marginTop: "15px" }} disabled={!productClick} onClick={handleSubmit}>Show Value</button>
+      <h2>{productSubmit}</h2>
     </div>
   );
 }
